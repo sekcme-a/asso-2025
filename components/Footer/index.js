41cd3b4f718a5909109cfb92dd1f5e67084e6c8d
송현서 <songmin21s@songhyeonseoui-MacBookPro.local>
2025-06-11 15:22:44 +0900

    asdf

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,14 +1,28 @@
-const Footer = () => {
+import { createServerSupabaseClient } from "@/utils/supabase/server";
+
+const Footer = async () => {
+  const supabase = await createServerSupabaseClient();
+  const { data: settings } = await supabase
+    .from("page_settings")
+    .select("data")
+    .eq("type", "main_footer")
+    .single();
+  const footerData = settings?.data || {};
   return (
     <footer className="mt-32 bg-gray-800 text-white py-8 px-20 flex justify-between items-center flex-col md:flex-row">
-      <div>
+      <div className="flex-1">
         <h6 className="text-3xl font-bold">대한생활체육회</h6>
 
-        <div className="text-sm">
+        <div className="text-xs whitespace-pre-line break-keep">
           <p className="text-sm mt-4">{`상호: (사)대한생활체육회 | 대표자명: 김균식`}</p>
           <p>고유번호 : 102-82-10135</p>
+          <p>{footerData.leftText}</p>
         </div>
       </div>
+      {/* <div className="flex-1">
+
+      </div>
+      <div className="flex-1"></div> */}
     </footer>
   );
 };
